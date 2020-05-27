2d4ef11e39f338d7547f6530318da3e03bbf3b37
Teny Peter <teny.peter007@gmail.com>
2020-05-27 15:49:44 +1000

    Initial commit

---
+++ conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['todo-spec.js'],
+    framework: 'jasmine2',
+    onPrepare: function() {
+      var AllureReporter = require('jasmine-allure-reporter');
+      jasmine.getEnv().addReporter(new AllureReporter({
+        resultsDir: 'allure-results'
+        
+      }));
+
+      jasmine.getEnv().afterEach(function(done){
+        browser.takeScreenshot().then(function (png) {
+          allure.createAttachment('Screenshot', function () {
+            return new Buffer(png, 'base64')
+          }, 'image/png')();
+          done();
+        })
+      });
+    }
+  };
